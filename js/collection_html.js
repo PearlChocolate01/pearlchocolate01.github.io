43ab42991f68a7e12dba05699123ad3d3abca273
zongyi <492605791@qq.com>
2023-02-09 14:44:45 +0800

    Site updated: 2023-02-09 14:44:43

--- js/collection_html.js
+++ js/collection_html.js
@@ -1,4 +1,10 @@
 document.write('<div id="root"><div class="container"><!-- 一个ani是一部作品 --><li class="ani" v-for="a in collection_data" :key="a.id"><div class="img_d"><img class="animation_img" :src="a.img_src"></div><div class="ani_info"><!-- {{a.name}} --><p class="ani_p" v-text="a.name"></p><p class="ani_p" v-text="a.author"></p><p class="ani_p" v-text="a.element"></p><p class="ani_p" v-text="a.lines"></p><p class="ani_p" v-text="a.time"></p><p class="ani_p" v-text="a.score"></p></div></li></div></div>');
+//document.write('<div id="root"><div class="coll-buttons"><button id="coll-year-all" class="coll-year" v-on:click="collButtonClick(e)">ALL</button><button id="coll-year-2022" class="coll-year" v-on:click="collButtonClick(e)">2022</button><button id="coll-year-2023" class="coll-year" v-on:click="collButtonClick(e)">2023</button></div><div class="container"><!-- 一个ani是一部作品 --><li class="ani" v-for="a in collection_data" :key="a.id" v-show="a.isShow"><div class="img_d"><img class="animation_img" :src="a.img_src"></div><div class="ani_info"><!-- {{a.name}} --><p class="ani_p" v-text="a.name"></p><p class="ani_p" v-text="a.author"></p><p class="ani_p" v-text="a.element"></p><p class="ani_p" v-text="a.lines"></p><p class="ani_p" v-text="a.time"></p><p class="ani_p" v-text="a.score"></p></div></li></div></div>');
+
+
+// function collButtonClick(e){
+// 	console.log(e.value)
+// }
 
 //展开格式：
 // <div id="root">
