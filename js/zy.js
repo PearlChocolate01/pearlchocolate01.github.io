07ba7a64fd8982f63b22a6dc1720d620b9d6e3a0
zongyi <492605791@qq.com>
2023-02-16 21:25:57 +0800

    Site updated: 2023-02-16 21:25:54

--- js/zy.js
+++ js/zy.js
@@ -1,4 +1,16 @@
-var full_page=document.getElementsByClassName("full_page");
-if(full_page.length!=0){
-	full_page[0].style.background="transparent";
-}
+// var full_page=document.getElementsByClassName("full_page");
+// if(full_page.length!=0){
+// 	full_page[0].style.background="transparent";
+// }
+
+console.log(`
+▕╮╭┻┻╮╭┻┻╮╭▕╮╲ 
+▕╯┃╭╮┃┃╭╮┃╰▕╯╭▏
+▕╭┻┻┻┛┗┻┻┛    ▏
+▕╰━━━┓┈┈┈╭╮▕╭╮▏
+▕╭╮╰┳┳┳┳╯╰╯▕╰╯▏
+▕╰╯┈┗┛┗┛┈╭╮▕╮┈▏
+`);
+
+//console.log($("iframe"))
+//$("#zy_iframe").contents().find("a").css('cursor', '../css/mouse/ania/ania_link.cur');
